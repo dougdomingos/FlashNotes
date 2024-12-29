a9bc0877ed8900b66835cf083b0a9b44ceaa903b
Douglas Domingos <doug.domingossilva@gmail.com>
2024-12-29 09:38:13 -0300

    refactor: simplify path to styles.css file

--- src/entrypoints/sidepanel/main.tsx
+++ src/entrypoints/sidepanel/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './../../assets/css/styles.css';
+import '~/assets/css/styles.css';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
