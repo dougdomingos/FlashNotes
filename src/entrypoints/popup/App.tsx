e04c89cd57ad1218fe7ece3f013e28a7bed8f690
Douglas Domingos <doug.domingossilva@gmail.com>
2024-12-30 22:04:22 -0300

    feat: add styles to popup to maintain consistency

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,13 +1,15 @@
 import Button from "@/components/Button";
 
-import { Pen, ClipboardText } from "@phosphor-icons/react";
+import { ClipboardText, Pen } from "@phosphor-icons/react";
 
 function App() {
   return (
-    <main className="flex flex-col items-center gap-2 px-2 py-4">
+    <main className="flex flex-col items-center gap-2 px-2 py-4 min-w-80">
       <header className="text-center">
         <h1 className="font-semibold">FlashNotes</h1>
-        <p className="">Write notes while you browse, as quick as a flash &#x26A1;</p>
+        <p className="text-md">
+          Write notes while you browse, as quick as a flash &#x26A1;
+        </p>
       </header>
       <div className="flex flex-row gap-2">
         <Button icon={Pen} label="New note" />
